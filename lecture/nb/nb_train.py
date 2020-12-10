e9184c53665c3a78c9e570745b5224b2786346d6
Ryota Eisaki <ryotaeisaki0530@icloud.com>
2020-12-10 18:28:04 +0900

    edit

--- lecture/nb/nb_train.py
+++ lecture/nb/nb_train.py
@@ -27,10 +27,7 @@ for line in open('train.cleaner_tf.txt', 'r'):
     line = line.replace(" \n", "\n")
     line_ = str_dic(line)
     space[tid] = line_
-    # print(space[tid])
     tid += 1
-
-# print(space[0]['まし'])
 
 pwj_cleaner = {}
 for id in space.keys():
@@ -44,12 +41,10 @@ all_words_cleaner = 0
 for id in pwj_cleaner.keys():
     tmp = pwj_cleaner[id] + delta
     all_words_cleaner = all_words_cleaner + tmp
-# print(all_words_cleaner)
 
 for id in pwj_cleaner.keys():
     tmp = pwj_cleaner[id] + delta
     pwj_cleaner[id] = tmp / all_words_cleaner
-
 
 with open('pwj_cleaner.txt', 'w') as f:
   print(pwj_cleaner, file=f)
@@ -62,10 +57,7 @@ for line in open('train.mp3player_tf.txt', 'r'):
     line = line.replace(" \n", "\n")
     line_ = str_dic(line)
     space[tid] = line_
-    # print(space[tid])
     tid += 1
-
-# print(space[0]['まし'])
 
 pwj_mp3player = {}
 for id in space.keys():
@@ -74,26 +66,15 @@ for id in space.keys():
             pwj_mp3player[val] = space[id][val]
         else:
             pwj_mp3player[val] = pwj_mp3player[val]+space[id][val]
-# print(pwj_mp3player)
-
-# for id in pwj_mp3player.keys():
-#     pwj_mp3player[id] = pwj_mp3player[id]/len(pwj_mp3player)
 
 all_words_mp3player = 0
 for id in pwj_mp3player.keys():
     tmp = pwj_mp3player[id] + delta
     all_words_mp3player = all_words_mp3player + tmp
-# print(all_words_cleaner)
-
-# print(all_words_mp3player)
 
 for id in pwj_mp3player.keys():
     tmp = pwj_mp3player[id] + delta
     pwj_mp3player[id] = tmp / all_words_mp3player
-
-# print(pwj_mp3player)
-# print(len(pwj_cleaner))
-# print(len(pwj_mp3player))
 
 with open('pwj_mp3player.txt', 'w') as f:
   print(pwj_mp3player, file=f)
