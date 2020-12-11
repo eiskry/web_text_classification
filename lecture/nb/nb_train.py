25fde6f8962617a8d622edbfd5dc4c36630ad186
Ryota Eisaki <ryotaeisaki0530@icloud.com>
2020-12-11 15:22:18 +0900

    nb_train

--- lecture/nb/nb_train.py
+++ lecture/nb/nb_train.py
@@ -7,14 +7,17 @@ import ast
 
 # ----- ----- ----- ----- ----- # 関数・deltaの定義
 
+# 特徴抽出結果を受け取り辞書に変換
 def str_dic(x):
     d = x
     d1 = '{' + '"' + d.replace(':', '":').replace(' ', ', "') + '}'
     d2 = ast.literal_eval(d1)
     return d2
 
-# smoothing
+# smoothing　有効
 delta = 1
+
+# smoothing を行わない場合
 # delta = 0
 
 # ----- ----- ----- ----- ----- # 教師文書の読み込み
@@ -28,6 +31,8 @@ for line in open('train.cleaner_tf.txt', 'r'):
     space[tid] = line_
     tid += 1
 
+# cleanerクラスについてP(wj|ci)を求める
+# 各単語の生起頻度を登録
 pwj_cleaner = {}
 for id in space.keys():
     for val in space[id].keys():
@@ -36,15 +41,18 @@ for id in space.keys():
         else:
             pwj_cleaner[val] = pwj_cleaner[val]+space[id][val]
 
+# cleanerクラスの全単語数を計算
 all_words_cleaner = 0
 for id in pwj_cleaner.keys():
     tmp = pwj_cleaner[id] + delta
     all_words_cleaner = all_words_cleaner + tmp
 
+# 最終的なP(wj|ci)を計算
 for id in pwj_cleaner.keys():
     tmp = pwj_cleaner[id] + delta
     pwj_cleaner[id] = tmp / all_words_cleaner
 
+# P(wj|ci)をファイルに出力
 with open('pwj_cleaner.txt', 'w') as f:
   print(pwj_cleaner, file=f)
 
@@ -58,6 +66,8 @@ for line in open('train.mp3player_tf.txt', 'r'):
     space[tid] = line_
     tid += 1
 
+# mp3playerクラスについてP(wj|ci)を求める
+# 各単語の生起頻度を登録
 pwj_mp3player = {}
 for id in space.keys():
     for val in space[id].keys():
@@ -66,14 +76,17 @@ for id in space.keys():
         else:
             pwj_mp3player[val] = pwj_mp3player[val]+space[id][val]
 
+# mp3playerクラスの全単語数を計算
 all_words_mp3player = 0
 for id in pwj_mp3player.keys():
     tmp = pwj_mp3player[id] + delta
     all_words_mp3player = all_words_mp3player + tmp
 
+# 最終的なP(wj|ci)を計算
 for id in pwj_mp3player.keys():
     tmp = pwj_mp3player[id] + delta
     pwj_mp3player[id] = tmp / all_words_mp3player
 
+# P(wj|ci)をファイルに出力
 with open('pwj_mp3player.txt', 'w') as f:
   print(pwj_mp3player, file=f)
