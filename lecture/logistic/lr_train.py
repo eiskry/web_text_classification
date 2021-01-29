e34312b14a0b5c5120689499a019037ca18096e5
Ryota Eisaki <ryotaeisaki0530@icloud.com>
2021-01-29 11:50:01 +0900

    edit

--- lecture/logistic/lr_train.py
+++ lecture/logistic/lr_train.py
@@ -47,31 +47,34 @@ def update_w(data, w, c):
             w[val] = (1 - lam * rho) * w[val] - r * data[id][val]
     return w
 
-
-def upedata_w_(data, w, r):
-    for id in data.keys():
-        for val in data[id].keys():
-            w[val] = (1 - lam * rho) * w[val] - r * data[id][val]
+# 各教師データについて重みの変化を計算
+def upedata_w_t(data_t, w, c):
+    value = 0
+    for val in data_t.keys():
+        value = value + data_t[value] * w[value]
+    logisig = my_function.logi_sig(value)
+    for val in data_t.keys():
+        change = rho * (logisig - c) * data_t[val]
+        if change < tol:
+            break
+        w[val] = (1 - lam * rho) * w[val] - r * data[val]
     return w
 
 # 重みの計算
-def calc_r(data, w, c):
-    for id in data.keys():
-        for val in data[id].keys():
-            value1  = data[id][val] * w[val]
-            value = value + value1
-        tmp = my_function.logi_sig(value)
-        r = rho * (tmp - c)
-    return r
+# def calc_r(data, w, c):
+#     for id in data.keys():
+#         for val in data[id].keys():
+#             value1  = data[id][val] * w[val]
+#             value = value + value1
+#         tmp = my_function.logi_sig(value)
+#         r = rho * (tmp - c)
+#     return r
 
 def update_w(data, w, c):
+    w_ = {}
     for id in data.keys():
-        for val in data[id][val].keys():
-            r = calc_r(data, w, c)
-            if value2 < tol:
-                break
-            w[val] = (1 - lam * rho) * w[val] - r * data[id][val]
-
+        w_ = upedata_w_t(data[id], w, c)
+        update_w(data, w_, c)
 
     # for id in data.keys():
     #     tmp = 0
