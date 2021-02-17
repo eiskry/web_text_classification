bae75d6ac12d7b391a35d1a8face935a02700795
Ryota Eisaki <ryotaeisaki0530@icloud.com>
2021-02-17 18:10:17 +0900

    edit

--- lecture/code/fea_extraction/test2.py
+++
@@ -1,5 +0,0 @@
-import math
-
-value = math.log(10)
-
-print(value)
--- lecture/jikken/2/data/open.py
+++
@@ -1,7 +0,0 @@
-import codecs
-
-fin  = codecs.open('test.txt', 'r', 'euc_jp')
-fout = codecs.open('text3.txt', 'w', 'shift_jis')
-
-for line in fin:
-    fout.write(line)
